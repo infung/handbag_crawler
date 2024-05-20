1cc5ed984739b6828f31399de1317a0f31c361c0
Yin Feng <y77feng@uwaterloo.ca>
2024-05-20 16:37:46 +0800

    May data

--- constant.py
+++ constant.py
@@ -1,6 +1,3 @@
 bag_brands = ['Valentino Garavani', 'Loewe', 'Cartier', 'Dior', 'Celine', 'Balenciaga', 'Chanel', 'Fendi', 'Gucci',
               'Hermes', 'Louis Vuitton', 'Prada', 'Saint Laurent', 'Bottega Veneta', 'Bvlgari', 'Chloe', 'Givenchy',
               'Goyard', 'Miu Miu']
-
-watch_brands = ['Rolex', 'IWC', 'Tudor', 'Cartier', 'Breitling', 'Omega', 'Panerai', 'Chopard', 'Patek Philippe',
-                'Audemars Piguet', 'Richard Mille', 'Jaeger-LeCoultre', 'Vacheron Constantin', 'Hublot']
